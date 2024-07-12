0ce5d545f074b027a9936ccbeed6288d01b26068
Val <valeryromano12315@gmail.com>
2024-07-12 14:04:22 -0400

    Initial Commit

---
+++ GithubTut2/GithubTut2App.swift
@@ -0,0 +1,17 @@
+//
+//  GithubTut2App.swift
+//  GithubTut2
+//
+//  Created by Scholar on 7/12/24.
+//
+
+import SwiftUI
+
+@main
+struct GithubTut2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
